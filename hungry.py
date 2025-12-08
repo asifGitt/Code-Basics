e9ed562fc59da42de2525b420ee1a5268d2a2c71
asifGitt <asif.githubb@gmail.com>
2025-12-08 12:27:29 +0530

    burger

--- hungry.py
+++ hungry.py
@@ -2,9 +2,10 @@ hungry = input("Are you hungry? ")
 
 if (hungry == "y"):
     print("Eat Pizza !!!")
+    print("Eat Burger !!!")
 
     thirsty = input("Are you thirsty? ")
-    if (thirsty):
+    if (thirsty == "y"):
         print("Drink Water !!!")
         print("Drink Juice !!!")
         
