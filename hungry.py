5cb20363c59e00212877823c6e261122f047a3c2
asifGitt <asif.githubb@gmail.com>
2025-12-09 01:18:16 +0530

    Added yes

--- hungry.py
+++ hungry.py
@@ -1,12 +1,12 @@
 hungry = input("Are you hungry? ")
 
-if (hungry == "y") or (hungry == "Y"):
+if (hungry == "y") or (hungry == "yes"):
     print("Eat Pizza !!!")
     print("Eat Burger !!!")
     print("Eat Fries !!!")
 
     thirsty = input("Are you thirsty? ")
-    if (thirsty == "y") or (thirsty == "Y"):
+    if (thirsty == "y") or (thirsty == "yes"):
         print("Drink Water !!!")
         print("Drink Juice !!!")
         
